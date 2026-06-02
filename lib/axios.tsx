08a1f5dc6086f84bd73990df04cc3aa3b1d72f55
rahul <rahul@gpoplus.com>
2026-06-02 21:49:06 +0530

    production

--- lib/axios.tsx
+++ lib/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:1100",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.prismplus.ai",
   withCredentials: true, // cookie automatically send hogi
   headers: {
     "Content-Type": "application/json",
